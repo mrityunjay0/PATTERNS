9c14c5fd293d33407fae82cb57f87c544f65e40f
mrityunjay <mrityunjaykmr.mk@gmail.com>
2025-02-25 23:41:35 +0530

    Reverse Numbered Pyramid

---
+++ Number_inc_Pyramid_Reverse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Number_inc_Pyramid_Reverse {
+
+    public static void pyramid(int n){
+        for(int i = n;i>=1;i--){
+            for(int j = 1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n");
+        int n = sc.nextInt();
+        pyramid(n);
+    }
+}
