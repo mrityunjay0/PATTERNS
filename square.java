d590e0cf025b0cbd94d4d35abf53cebdcb9b43a5
mrityunjay <mrityunjaykmr.mk@gmail.com>
2025-02-25 22:35:02 +0530

    Square

--- square.java
+++ square.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class square{
     public static void main(String args[]){
-        System.out.println("hello");
+        int n = 5;
+        for(int i = 0;i<5;i++){
+            for(int j = 0;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
