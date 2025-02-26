6fd354af4973f382b90e9b188094ac6a79ee22cd
mrityunjay <mrityunjaykmr.mk@gmail.com>
2025-02-26 23:25:21 +0530

    Reverse Pyramid

---
+++ Star_Reverse_pyramid.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Star_Reverse_pyramid {
+
+    public static void pyramid(int n){
+        for(int i = n-1;i>=0;i--){
+            for(int j = 0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k = 0;k<2*i+1;k++){
+                System.out.print("*");
+            }
+            for(int l = 0;l<n-i-1;l++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER N:");
+        int n = sc.nextInt();
+        pyramid(n);
+    }
+}
+
+
+// *************
+//  *********** 
+//   *********  
+//    *******   
+//     *****    
+//      ***     
+//       *   
